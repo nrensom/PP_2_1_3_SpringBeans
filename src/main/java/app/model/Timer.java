f3336afce88b2621e08b3f92f6a70d6e8130461d
Anastasia Rensom <nrensom666@gmail.com>
2022-06-01 20:55:03 +0300

    //

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,8 +1,11 @@
 package app.model;
 
-public class Timer {
+import org.springframework.stereotype.Component;
 
+@Component
+public class Timer {
     private Long nanoTime = System.nanoTime();
+
 
     public Long getTime() {
         return nanoTime;
